844a67f556e8eabdcf53e35e29e59e895e32291a
kikro1 <bogdash.voitencov.2008@gmail.com>
2024-12-04 11:06:29 +0000

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import Product from "./Product";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
+
+
+const App =() => {
+  return (
+    <div>
+      <h2>heading lalal</h2>
+      <button>click</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Product.jsx
@@ -0,0 +1,11 @@
+// src/Product.jsx
+
+export default function Product() {
+    return (
+    <div>
+    <h2>some changes</h2>
+          <p>Price: 999 credits</p>
+    </div>
+    );
+    };
+    
